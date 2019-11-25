bcc496f54ec7e1d76a9677b1794b60b423107880
Alex Khmilevoi <khmilevoi@gmail.com>
2019-11-25 16:13:37 +0200

    heroku deploy

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 import cityList from "./city.list.min.json.js";
 import { DataBase } from "./DataBase.js";
@@ -6,13 +7,12 @@ import { DataBase } from "./DataBase.js";
 const db = new DataBase(cityList, "name");
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.get("/cities", (req, res) => {
   const { name, count = 20 } = req.query;
 
-  res.setHeader("Access-Control-Allow-Origin", `*`);
+  res.setHeader("Access-Control-Allow-Origin", *);
 
   if (typeof name === "undefined" || isNaN(+count) || count < 1) {
     res.status(400).send();
@@ -27,6 +27,14 @@ app.get("/cities", (req, res) => {
   }
 });
 
+const staticPath = path.join(path.resolve(), "build");
+
+app.use(express.static(staticPath));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`start listening on ${PORT}`);
+  console.log(start listening on ${PORT});
 });

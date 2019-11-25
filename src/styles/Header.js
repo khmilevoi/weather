d7005a9ff60add17a860b544a90a5dc639adf21c
Alex Khmilevoi <khmilevoi@gmail.com>
2019-11-25 11:11:25 +0200

    style(Header) add paper

--- src/styles/Header.js
+++ src/styles/Header.js
@@ -4,11 +4,4 @@ import { Container as C } from "styles/Index";
 
 export const Container = styled(C)`
   min-height: 215px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #90caf9;
-  border-radius: 4px;
-  margin: 30px auto;
-  padding: 0 15px;
 `;
